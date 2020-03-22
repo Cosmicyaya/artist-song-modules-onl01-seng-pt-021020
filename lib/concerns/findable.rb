de0234228437f5b6722f2e7ddb4a3d074d8e01bf
Ryan Hollins <ballbaroi@gmail.com>
2020-03-22 13:39:16 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,7 @@
 module find_by_name
   def find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    @artists.detect{|a| a.name == name}
     
-    
+    def find_by_name(song)
+      @song.detect{|a| a.name == name}
 end
